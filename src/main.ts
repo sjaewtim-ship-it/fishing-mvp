efb8bdf39cfef1e42fb0243b05cdb0cc0fe9adfd
知止 <zhizhi@zhizhideMBP.lan>
2026-03-22 14:31:31 +0800

    change phaser scale to envelop

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,22 @@ import Phaser from 'phaser';
 import { MainScene } from './game/MainScene';
 import { FishingScene } from './game/FishingScene';
 import { ResultScene } from './game/ResultScene';
-import { MobileHelper } from './game/MobileHelper';
+import { WeChatHelper } from './game/WeChatHelper';
 
-MobileHelper.ensurePortraitOverlay();
+WeChatHelper.setup();
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   parent: 'game-container',
-  backgroundColor: '#000000',
+  backgroundColor: '#8fd3ff',
+  input: {
+    activePointers: 3,
+    touch: {
+      capture: true,
+    },
+  },
   scale: {
-    mode: Phaser.Scale.FIT,
+    mode: Phaser.Scale.ENVELOP,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 750,
     height: 1334,
